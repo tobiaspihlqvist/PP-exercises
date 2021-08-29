49bb301a9d619b4c9aca20a0c3129171b0f66b4f
tobiaspihlqvist <tobias.pihlqvist@hiq.se>
2021-08-29 17:03:20 +0200

    init

---
+++ exercise-4/app.py
@@ -0,0 +1,36 @@
+import turtle
+import sys
+
+def Main(*args):
+    instance = turtle.Turtle;
+        # for arg in sys.argv[1:]:
+        #     return;
+    # instance.pensize(2);
+    # instance.
+
+    print("hello world");
+
+def CommandInterpreter(command, unit):
+    dict = {
+    "U": {"args": False, "cmd": "penup" },
+    "D": {"args": False, "cmd": "pendown" },
+    "W": {"args": True, "cmd": "left" },
+    "N": {"args": True, "cmd": "up" },
+    "S": {"args": True, "cmd": "down" },
+    "E": {"args": True, "cmd": "right" },
+    };
+    dict.get["P"];
+
+#Hitta kommando. if number släng in det också
+
+if __name__ == "__Main__":
+    Main()
+
+
+#p 2 select pen 2
+#d pen down
+# w draw west 2cm
+# n 
+# e
+# s
+# u pen up 
