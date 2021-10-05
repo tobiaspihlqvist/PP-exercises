bd2d047bd329cba191259291a13e7bd9fb411259
tobiaspihlqvist <tobias.pihlqvist@hiq.se>
2021-10-05 22:22:11 +0200

    added try catch and show minutes after midnight

--- exercise-6-8/exercise_7/src/main.ts
+++ exercise-6-8/exercise_7/src/main.ts
@@ -4,8 +4,16 @@ const button = document.querySelector<HTMLDivElement>("#parsingButton")!;
 
 function tryParse() {
 	const inputVal = document.querySelector<HTMLInputElement>("#parse")!.value;
-	const res = grammar.parse(inputVal);
-	console.log(res);
+	const outputField = document.getElementById("output");
+	try {
+		const res = grammar.parse(inputVal);
+		//@ts-ignore
+		outputField.innerHTML = `the time input you provided is ${res} minutes after midnight`;
+	} catch (e) {
+		//@ts-ignore
+		outputField.innerHTML = "that is not a valid timeformat";
+		console.log(e, "errorrrr");
+	}
 }
 
 button.onclick = function () {
